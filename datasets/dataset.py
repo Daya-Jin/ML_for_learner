da75d0cbf604cfe98e91fe1bf90a4bbe9d5c754d
Daya_Jin <qq435248055@gmail.com>
2019-05-16 22:44:50 +0800

    Update dataset.py

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -5,11 +5,6 @@ import os
 data_structure = namedtuple('data_structure', ['data', 'target'])
 
 
-# 路径问题，注意在外部引用并执行load函数时，相对路径的当前路径是以__main__为准的
-# 这里的路径写法只能用于算法模块的测试逻辑，实际是存在BUG的
-# 一种解决方法是使用绝对路径，但是不具有通用性，待解决
-
-
 def load_boston():
     path = os.path.join(os.path.dirname(__file__), 'housing', 'housing.data')
     df = pd.read_csv(path, header=None)
